df62a58e66a03414448777e095bc2e51c6cfdb17
felix-caba <felixcaba@proton.me>
2025-01-27 14:31:48 +0100

    Implement initial MPI setup and add functionality to read source IPs from a file; enhance PortScanner class with port scanning and OS detection methods.

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,5 +8,40 @@
 #include <mutex>
 #include <chrono>
 #include <cstring>
+#include <fstream>
 
 
+std::vector<std::string> source_ips;
+std::unordered_map<std::string, std::vector<std::string>> dest_file;
+
+int main(int argc, char **argv)
+{
+    int rank, n_ranks;
+    MPI_Init(&argc, &argv);
+
+    // For each line on the file source_ips, add the IP to the source_ips vector and also add one nrank
+
+     if (rank == 0) {
+        std::ifstream file("../source_ips.txt");
+        if (file.is_open()) {
+            std::string line;
+            while (getline(file, line)) {
+                source_ips.push_back(line);
+            }
+            file.close();
+        }
+    }
+
+
+
+    
+
+
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
+
+
+
+
+}
--- src/Scanner.h
+++ src/Scanner.h
@@ -8,27 +8,106 @@
 #include <unordered_map>
 #include "ports.h"
 
-
+// declaro
 class PortScanner
 {
-
 public:
-    PortScanner(const std::string &ipAddress) : ip(ipAddress), 
-    portServiceMap(PORT_SERVICE_MAP), io_context(), resolver(io_context)
+    PortScanner(const std::string &ipAddress) : ip(ipAddress), portServiceMap(PORT_SERVICE_MAP), io_context(), resolver(io_context)
     {
+    }
 
-       
+    void scan()
+    {
+        std::vector<std::thread> threads;
+        for (const auto &port : PORT_SERVICE_MAP)
+        {
+            threads.emplace_back(&PortScanner::scanPort, this, port.first);
+        }
 
+        for (auto &t : threads)
+        {
+            t.join();
+        }
+    }
 
+    std::unordered_map<int, std::string> getOpenPorts() const
+    {
+        return open_ports;
+    }
+
+    int getClosedPorts() const
+    {
+        return closedPorts;
+    }
+
+    std::string getOperatingSystemX() const
+    {
+        return operating_system;
     }
 
 private:
+    // Atributos
     std::string ip;
     const std::unordered_map<int, std::string> &portServiceMap;
     std::unordered_map<int, std::string> open_ports;
     std::string operating_system;
+    std::mutex port_mutex;
+    int closedPorts = 0;
+
     boost::asio::io_context io_context;
     boost::asio::ip::tcp::resolver resolver;
-   
-};
 
+    std::string getOperatingSystem()
+    {
+    }
+
+    void scanPort(int port)
+    {
+
+        try
+        {
+
+            boost::asio::ip::tcp::socket socket(io_context);
+            boost::asio::ip::tcp::endpoint endpoint(
+                boost::asio::ip::make_address(ip), port);
+
+            boost::system::error_code error;
+            socket.connect(endpoint, error);
+
+            if (!error)
+            {
+                // bloquea la edicion del puerto mutex, asegura que sea
+                // desbloqueado al terminar la edicion
+
+                // el mutex hace que solo lo debajo de el se bloquee, hasta
+                // que salga del scope, osea, ya terminado
+                std::lock_guard<std::mutex> lock(port_mutex);
+
+                // findea en el map el puerto que he escaneado
+
+                auto service = PORT_SERVICE_MAP.find(port);
+
+                // port service map es basicamente dentro del iterador,
+                // lo que significa que ha llegado al final. si es end, es que
+                // no lo ha encontrado, si no es end, es que ha salido guay
+                // y le dice que coja el segundo valor que es la valor asociado a la key
+                if (service != PORT_SERVICE_MAP.end())
+                {
+                    open_ports[port] = service->second;
+                }
+                else
+                {
+                    open_ports[port] = "Unknown";
+                }
+            }
+            else
+            {
+                closedPorts++;
+            }
+        }
+        catch (std::exception &e)
+        {
+            ++closedPorts;
+        }
+    }
+};
