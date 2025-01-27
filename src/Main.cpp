722e161530271251581599b4eaaa81963b093083
felix-caba <felixcaba@proton.me>
2025-01-27 18:24:40 +0100

    Implement port scanning and result aggregation across MPI ranks

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,6 +9,7 @@
 #include <chrono>
 #include <cstring>
 #include <fstream>
+#include "Scanner.h"
 
 std::vector<std::string> source_ips;
 std::unordered_map<std::string, std::vector<std::string>> dest_file;
@@ -26,7 +27,7 @@ int main(int argc, char **argv)
 
     if (rank == 0)
     {
-        std::ifstream file("../source_ips.txt");
+        std::ifstream file("./source_ips.txt");
         if (file.is_open())
         {
             std::string line;
@@ -79,16 +80,68 @@ int main(int argc, char **argv)
         delete[] buffer;
     }
 
-
     ips_per_rank = floor(num_ips / n_ranks);
 
     // Process each IP with a rank
 
     for (int i = rank * ips_per_rank; i < (rank * ips_per_rank) + ips_per_rank; i++)
     {
-   
+
+        PortScanner scanner(source_ips[i]);
+        scanner.scan();
+        std::unordered_map<int, std::string> open_ports = scanner.getOpenPorts();
+        int closed_ports = scanner.getClosedPorts();
+
+        std::string ip = source_ips[i];
+
+        std::string open_ports_str = "";
+
+        for (const auto &port : open_ports)
+        {
+            open_ports_str += std::to_string(port.first) + " ";
+        }
+
+        std::string closed_ports_str = std::to_string(closed_ports);
+
+        std::string result = open_ports_str + " " + closed_ports_str + " ";
+
+        dest_file[ip] = {result};
     }
 
+    if (rank != 0)
+    {
+        std::string data_to_send;
+        for (const auto &entry : dest_file)
+        {
+            data_to_send += entry.first + ":" + entry.second[0] + "\n";
+        }
 
+        int data_size = data_to_send.size();
+        MPI_Send(&data_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(data_to_send.c_str(), data_size, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
+    else
+    {
+        std::ofstream outfile("./results.txt");
+        // Write the results of rank 0 root
+        for (const auto &entry : dest_file)
+        {
+            outfile << entry.first << ":" << entry.second[0] << "\n";
+        }
 
+        // Other ranks
+        for (int other_rank = 1; other_rank < n_ranks; other_rank++)
+        {
+            int data_size;
+            MPI_Recv(&data_size, 1, MPI_INT, other_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            char *buffer = new char[data_size + 1];
+            MPI_Recv(buffer, data_size, MPI_CHAR, other_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            buffer[data_size] = '\0';
+
+            outfile << buffer;
+            delete[] buffer;
+        }
+        outfile.close();
+    }
 }
